04cc145a8b1d1963e6ee55c3c21bd217926591ca
18100262 <adnanfootball722@gmail.com>
2017-04-18 00:07:48 +0500

    first commit

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,10 @@
-#include <iostream> 
-int main() 
-{ 
-  std::cout << "Hello World!\n"; 
-  return 0; 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello World!\n";
+  cout << "This is second line";
+  cout << "This is third line";
+  return 0;
 }
